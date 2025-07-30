2e129db07427bbce8636ba1dc7ccb67d80eb4c78
OldMemory1 <3558066700@qq.com>
2025-07-31 07:47:27 +0800

    init

---
+++ src/test/java/org/example/installerbyspringboot/InstallerBySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.installerbyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstallerBySpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
